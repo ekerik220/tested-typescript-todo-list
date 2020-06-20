3f6267d08ced054795f165d817294d37d9669ec2
Evan K <ekerik220@gmail.com>
2020-06-20 10:31:09 +0900

    Added background gradient to body

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { createGlobalStyle } from "styled-components";
 
 function App() {
-  return <div></div>;
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      <div></div>
+    </React.Fragment>
+  );
 }
 
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #2C3E50;  /* fallback for old browsers */
+    background: -webkit-linear-gradient(to right, #4CA1AF, #2C3E50);  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to right, #4CA1AF, #2C3E50); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  }
+`;

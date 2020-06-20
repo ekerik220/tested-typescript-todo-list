c0d1ce9ea6cfc7010b06322d4bc4ab3e2aadb648
Evan K <ekerik220@gmail.com>
2020-06-20 14:09:40 +0900

    Added tests to check if TodoAdder is rendering

---
+++ src/components/TodoAdder/TodoAdder.test.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import TodoAdder from "./TodoAdder";
+
+it("has placeholder text 'Enter new to-do!'", () => {
+  const { getByPlaceholderText } = render(<TodoAdder />);
+  const placeHolderText = getByPlaceholderText("Enter new to-do!");
+  expect(placeHolderText).toBeInTheDocument();
+});
+
+it("renders a button", () => {
+  const { getByRole } = render(<TodoAdder />);
+  const button = getByRole("button");
+  expect(button).toBeInTheDocument();
+});

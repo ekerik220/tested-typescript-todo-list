23e1ed86127ec117593339c7541bbbd273d0961c
Evan K <ekerik220@gmail.com>
2020-06-20 13:53:04 +0900

    Finished TodoAdder component layout

---
+++ src/components/TodoAdder/TodoAdder.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import styled from "styled-components";
+
+function TodoAdder() {
+  return (
+    <Container style={{ display: "flex" }}>
+      <TodoInput placeholder="Enter new to-do!" />
+      <AddButton>
+        <i className="fas fa-plus"></i>
+      </AddButton>
+    </Container>
+  );
+}
+
+export default TodoAdder;
+
+const Container = styled.div`
+  position: absolute;
+  transition: transform 0.2s;
+
+  &:focus-within {
+    transform-origin: center center;
+    transform: scale(1.2);
+  }
+`;
+
+const TodoInput = styled.input`
+  border-radius: 5px 0 0 5px;
+  border: 0;
+  padding-left: 5px;
+`;
+
+const AddButton = styled.button`
+  background: #af5a4c;
+  border: 0;
+  border-radius: 0 5px 5px 0;
+  transition: box-shadow 0.2s;
+  cursor: pointer;
+
+  &::-moz-focus-inner {
+    border: 0;
+  }
+
+  &:focus {
+    box-shadow: 0 0 0 2px rgba(219, 103, 83, 0.7);
+  }
+`;

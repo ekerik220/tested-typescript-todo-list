3a56c3cbe915cfb3c1a9741eb3acc68850afb664
Evan K <ekerik220@gmail.com>
2020-06-22 16:35:27 +0900

    Add layout styled-components and plug in TodoAdder

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,26 @@
 import React from "react";
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
+import TodoAdder from "../TodoAdder/TodoAdder";
 
 function App() {
   return (
     <React.Fragment>
       <GlobalStyle />
-      <div></div>
+      <PageLayout>
+        <Container justify="center">
+          <TodoAdder />
+        </Container>
+      </PageLayout>
     </React.Fragment>
   );
 }
 
 export default App;
+
+interface ContainerProps {
+  width?: string;
+  justify?: string;
+}
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -18,4 +28,14 @@ const GlobalStyle = createGlobalStyle`
     background: -webkit-linear-gradient(to right, #4CA1AF, #2C3E50);  /* Chrome 10-25, Safari 5.1-6 */
     background: linear-gradient(to right, #4CA1AF, #2C3E50); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
   }
+`;
+
+const PageLayout = styled.div`
+  display: grid;
+`;
+
+const Container = styled.div<ContainerProps>`
+  display: flex;
+  justify-content: ${(props) => props.justify || "flex-start"};
+  width: ${(props) => props.width || "100%"};
 `;

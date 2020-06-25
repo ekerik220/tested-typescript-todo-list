42a666676a0ca986c728865c27a6eaabebd60d95
Evan K <ekerik220@gmail.com>
2020-06-25 13:54:35 +0900

    Added reminder comment

--- src/components/TodoList/useTodoList.tsx
+++ src/components/TodoList/useTodoList.tsx
@@ -4,3 +4,8 @@ export function useTodoList() {
   const todoList = useSelector((state: RootState) => state.todoList);
   return { todoList };
 }
+
+/* 
+  Nothing to really test here, but if you add more functionality 
+  please add a test file for this 
+*/

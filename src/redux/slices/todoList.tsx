99bf46263534971ea7058114944069cdc82bd51e
Evan K <ekerik220@gmail.com>
2020-06-25 09:09:51 +0900

    Added toggleTodoItemDoneState and gave existing actions proper TodoItem typing

--- src/redux/slices/todoList.tsx
+++ src/redux/slices/todoList.tsx
@@ -4,15 +4,30 @@ const todoList = createSlice({
   name: "todoList",
   initialState: [],
   reducers: {
-    addTodoItem: (state: object[], action) => {
+    addTodoItem: (state: TodoItem[], action: { payload: TodoItem }) => {
       state.push(action.payload);
     },
-    deleteTodoItem: (state: object[], action) => {
-      const index = state.findIndex(action.payload);
+    deleteTodoItem: (state: TodoItem[], action: { payload: TodoItem }) => {
+      const index = state.findIndex(
+        (todoItem) => todoItem.task === action.payload.task
+      );
       state.splice(index, 1);
+    },
+    toggleTodoItemDoneState: (
+      state: TodoItem[],
+      action: { payload: TodoItem }
+    ) => {
+      const index = state.findIndex(
+        (todoItem) => todoItem.task === action.payload.task
+      );
+      state[index].done = !state[index].done;
     },
   },
 });
 
-export const { addTodoItem, deleteTodoItem } = todoList.actions;
+export const {
+  addTodoItem,
+  deleteTodoItem,
+  toggleTodoItemDoneState,
+} = todoList.actions;
 export default todoList.reducer;
